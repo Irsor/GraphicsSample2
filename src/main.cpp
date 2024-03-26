4c52a4e0cdcfd13df788e0a335554f4db0e122c7
Андрей Васильев <skar67@yandex.ru>
2024-03-26 21:06:05 +0300

    Добавлены шрифты и изображения

--- src/main.cpp
+++ src/main.cpp
@@ -45,12 +45,28 @@ int main(void)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
+    io.Fonts->AddFontFromFileTTF("../assets/fonts/Wolgadeutsche.otf", 20, nullptr, io.Fonts->GetGlyphRangesCyrillic());
     ImGui::StyleColorsDark();
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 460 core");
 
     bool checkBoxValue = false;
+
+    // Загрузка изображения.
+    int imageWidth;
+    int imageHeight;
+    int channels;
+    unsigned char* imageData = stbi_load("../assets/images/hearths.jpg", &imageWidth, &imageHeight, &channels, STBI_rgb_alpha);
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageWidth, imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -69,6 +85,12 @@ int main(void)
         ImGui::SetNextWindowSize(ImVec2(fb_width, fb_height));
         ImGui::Begin("My Fullscreen Window", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
        
+        ImGui::Button("Авторизация", ImVec2(186, 29));
+
+        // Отображение изображения с помощью ImGui::Image()
+        ImGui::Image((void*)(intptr_t)textureID, ImVec2(imageWidth / 10, imageHeight / 10));
+
+
         ImGui::End();
         ImGui::Render();
 
@@ -86,6 +108,7 @@ int main(void)
         glfwPollEvents();
     }
 
+    stbi_image_free(imageData);
     glfwTerminate();
     return 0;
 }
